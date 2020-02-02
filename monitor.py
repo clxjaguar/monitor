16eb10c2e2fe2c5b9845b36907adf8b36a4ea104
cLx <clx.kat@gmail.com>
2020-02-02 23:16:43 +0100

    Pushing my code. It's a start. Just a start.

---
+++ monitor.py
@@ -0,0 +1,445 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# sudo apt-get install python-pip python-pyside python-setuptools python-pyqtgraph
+# pip install --user --upgrade pip
+# pip install --user --upgrade pyserial
+
+import os, sys, time, math, threading
+
+def pipRun(command):
+	print("$ "+command)
+	os.system(command)
+
+pipInstallRan = False
+def pipInstall(package):
+	global pipInstallRan
+	print("We want to install: "+package)
+	if not pipInstallRan:
+		pipInstallRan = True
+		pipRun(sys.executable+" -m pip install --user --upgrade pip")
+		pipRun(sys.executable+" -m pip install --user setuptools")
+	pipRun(sys.executable+" -m pip install --user "+package)
+
+try:
+	from PyQt4.QtGui import *
+	from PyQt4.QtCore import *
+except:
+	pipInstall("PyQt4")
+
+try:
+	import numpy
+except:
+	pipInstall("numpy")
+
+try:
+	import pyqtgraph # needs import PyQt4 before!
+except:
+	pipInstall("pyqtgraph")
+
+try:
+	import serial
+except:
+	pipInstall("pyserial")
+
+if pipInstallRan:
+	cmd = sys.executable+' '+' '.join(sys.argv)
+	print("Re-exec: "+cmd)
+	time.sleep(2)
+	os.system(cmd)
+	time.sleep(1)
+	exit(-1)
+
+import usb_oximeter
+
+class Parameter():
+	def __init__(self, name, unit, color, format, vmin, vmax, plotid=0):
+		self.name = name
+		self.unit = unit
+		self.color = color
+		self.format = format
+		self.value = float("nan")
+		self.min = vmin
+		self.max = vmax
+		self.plotid = plotid
+		self.info = ""
+		self.trends = []
+
+	def updateValue(self, value):
+		self.value = value
+
+	def copyValueToTrends(self):
+		self.trends.append(self.value)
+
+	def getValueStr(self):
+		if (self.value != self.value):
+			return('---')
+		else:
+			return(self.format % self.value)
+
+PR = 0; SPO2 = 1; TEMP = 2
+parameters = {}            # name     unit     color     format min  max  plotid
+parameters[PR]   = Parameter(u'PR',   u'bpm', '#00d000', "%.0f", 50, 120, 0) # #87CEFA
+parameters[SPO2] = Parameter(u'SpO₂', u'%',   '#50CCFF', "%.0f", 93, 100, 1)
+#parameters[TEMP] = Parameter(u'TEMP', u'°C',  '#FF5000', "%.1f", 36,  38, 2)
+#parameters[TEMP].updateValue(39.15)
+
+X_fast_oximeter = range(200); Y_fast_oximeter = 200*[0]; Y_fast_oximeter_index = 0; beat = False
+def OximeterCallback(SpO2, pulse_rate):
+	global oxi1, m1, beat, parameters
+	global X_fast_oximeter, Y_fast_oximeter, Y_fast_oximeter_index
+
+	if (SpO2 != 0):
+		parameters[SPO2].updateValue(SpO2)
+	else:
+		parameters[SPO2].updateValue(float('nan'))
+	if (pulse_rate != 0):
+		parameters[PR].updateValue(pulse_rate)
+	else:
+		parameters[PR].updateValue(float('nan'))
+
+	if (oxi1.SpO2dropping):
+		parameters[PR].info = ""
+		parameters[SPO2].info = "SPO2 DROPPING!"
+		m1.div_refresh_values = 0
+	elif (oxi1.probeError):
+		parameters[PR].info = "PROBE ERROR"
+		parameters[SPO2].info  = "PROBE ERROR"
+		m1.div_refresh_values = 0
+	elif (oxi1.searchingTooLong):
+		parameters[PR].info = "SEARCHING!"
+		parameters[SPO2].info = "SEARCHING!"
+		m1.div_refresh_values = 0
+	elif (oxi1.searching):
+		parameters[PR].info = "SEARCHING"
+		parameters[SPO2].info = "SEARCHING"
+	else:
+		parameters[PR].info = ""
+		parameters[SPO2].info = str('S'+str(oxi1.signalStrength)+' '+oxi1.bargraphValue*'|')
+
+	if (oxi1.pulse):
+		beat = True
+		m1.div_refresh_values = 0
+
+	# update data for fast curve ...
+	try:
+		Y_fast_oximeter[Y_fast_oximeter_index] = oxi1.waveformValue
+		Y_fast_oximeter_index+=1
+		Y_fast_oximeter[Y_fast_oximeter_index] = float('nan')
+	except:
+		Y_fast_oximeter[0] = oxi1.waveformValue
+		Y_fast_oximeter_index=1
+
+class MonitorGUI(QWidget):
+	def __init__(self):
+		super(MonitorGUI, self).__init__()
+		self.trends_time = []
+		self.div_refresh_values = 0
+		self.initUI()
+
+	def initUI(self):
+		self.setStyleSheet("\
+			QWidget { background-color: #000000; color: #ffffff; } \
+			QSplitter::handle:vertical   { image: none; } \
+			QSplitter::handle:horizontal { width:  2px; image: none; } \
+			QPushButton { background-color: #404040; } \
+			QLabel#c_names { font-size: 30pt; } \
+			QLabel#c_units { font-size: 20pt; } \
+			QLabel#c_limits { font-size: 10pt; } \
+			QLabel#c_values { font-size: 70pt; margin: 0px; padding: 0px; } \
+			QLabel#c_infos { font-size: 15pt; } \
+			QLabel#clockLabel { color: #707070; font-size: 10pt; } \
+			QLabel#clock { color: #ffffff; font-size: 15pt; } \
+			QGroupBox { border: 1px solid #707070; border-radius: 8px; background-color: #000000; } \
+		");
+
+		# elements in the left part of the screen
+		left_layout = QVBoxLayout()
+		splitter1 = QSplitter(Qt.Vertical)
+
+		splitter1.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+		left_layout.addWidget(splitter1)
+
+		# pyqtgraph curves
+		self.oximeter = pyqtgraph.PlotWidget()
+		self.oximeter.showAxis('bottom', show=False)
+		self.oximeter.showAxis('left', show=False)
+		splitter1.addWidget(self.oximeter)
+
+		plotids = []
+		for i in parameters:
+			plotids.append(parameters[i].plotid)
+
+		self.trendsPw = []
+		for i in plotids:
+			axis = DummyAxis
+			if i == len(plotids) - 1: axis = DateAxis
+			pw = pyqtgraph.PlotWidget(axisItems={'bottom': axis(orientation='bottom')})
+			if i > 0:
+				pw.setXLink(self.trendsPw[0])
+			pw.getViewBox().setMouseMode(pw.getViewBox().RectMode) # one button mode
+			pw.showGrid(x=True, y=True)
+			splitter1.addWidget(pw)
+			self.trendsPw.append(pw)
+
+		for i in parameters:
+			self.trendsPw[parameters[i].plotid].setLabel('left', text=parameters[i].name, units=parameters[i].unit)
+
+		# elements in the right part of the screen
+		right_layout = QVBoxLayout()
+
+		self.c_frames = {}
+		self.c_names = {}
+		self.c_units = {}
+		self.c_limits = {}
+		self.c_values = {}
+		self.c_infos = {}
+
+		for i in range(len(parameters)):
+			self.c_frames[i] = QGroupBox()
+			self.setFrameStyleSheet(i)
+			self.c_frames[i].setFlat(True);
+			vbox = QVBoxLayout(self.c_frames[i])
+			vbox.setSpacing(0)
+
+			hbox = QHBoxLayout()
+			self.c_names[i] = QLabel()
+			self.c_names[i].setObjectName('c_names')
+			self.c_names[i].setAlignment(Qt.AlignLeft | Qt.AlignBottom)
+			self.c_names[i].setText(parameters[i].name)
+			hbox.addWidget(self.c_names[i])
+
+			self.c_units[i] = QLabel()
+			self.c_units[i].setObjectName('c_units')
+			self.c_units[i].setAlignment(Qt.AlignRight | Qt.AlignBottom)
+			self.c_units[i].setText('  '+parameters[i].unit)
+			hbox.addWidget(self.c_units[i])
+			vbox.addLayout(hbox)
+
+			self.c_limits[i] = QLabel()
+			self.c_limits[i].setObjectName('c_limits')
+			self.c_limits[i].setAlignment(Qt.AlignRight | Qt.AlignBottom)
+			self.c_limits[i].setText(str(parameters[i].min)+"-"+str(parameters[i].max))
+			vbox.addWidget(self.c_limits[i])
+
+			self.c_values[i] = QLabel()
+			self.c_values[i].setObjectName('c_values')
+			self.c_values[i].setAlignment(Qt.AlignRight | Qt.AlignVCenter)
+			self.c_values[i].setText('-')
+
+			vbox.addWidget(self.c_values[i])
+
+			self.c_infos[i] = QLabel()
+			self.c_infos[i].setObjectName('c_infos')
+
+			self.c_infos[i].setText(parameters[i].info)
+			vbox.addWidget(self.c_infos[i])
+
+			right_layout.addWidget(self.c_frames[i])
+
+		right_layout.addStretch()
+
+		def makeButton(text, function):
+			btn = QPushButton(text)
+			btn.setFocusPolicy(Qt.TabFocus)
+			btn.clicked.connect(function)
+			return btn
+
+		# buttons
+		right_layout.addWidget(makeButton("Clear plots", self.btnReset))
+		right_layout.addWidget(makeButton("Autorange", self.btnAutoRange))
+		right_layout.addWidget(makeButton("Full/Window", self.btnFullScreen))
+		right_layout.addWidget(makeButton("Exit", self.close))
+
+		# clock
+		timeLabel = QLabel("Time")
+		timeLabel.setObjectName('clockLabel')
+		self.clockLabel = QLabel("--:--:--")
+		self.clockLabel.setObjectName('clock')
+		self.clockLabel.setAlignment(Qt.AlignRight)
+		gb = QGroupBox()
+		gb.setFlat(True);
+		gb.setObjectName('clock')
+		ly = QVBoxLayout(gb)
+		ly.addWidget(timeLabel)
+		ly.addWidget(self.clockLabel)
+		right_layout.addWidget(gb)
+
+		## entire screen
+		container_layout = QHBoxLayout(self)
+		container_layout.addLayout(left_layout)
+		container_layout.addLayout(right_layout)
+
+		## Display the widget as a new window
+		self.setWindowTitle(u"Monitoring")
+		self.setWindowState(self.windowState() ^ Qt.WindowMaximized)
+		self.showFullScreen()
+		self.show()
+		self.setCursor(Qt.BlankCursor)
+
+		# Timers
+		self.timerHideMouse = QTimer()
+		self.timerHideMouse.timeout.connect(self.timerHideMouseTimeout)
+
+		self.timerUpdateUi = QTimer()
+		self.timerUpdateUi.timeout.connect(self.timerUpdateUiTimeout)
+
+		self.timerClock = QTimer()
+		self.timerClock.timeout.connect(self.timerClockTimeout)
+		self.timerClockTimeout()
+
+		# Start timers
+		self.timerUpdateUi.start(50)
+		self.timerClock.start(1000)
+
+	def timerUpdateUiTimeout(self):
+		global beat
+		if (self.div_refresh_values > 0):
+			self.div_refresh_values-=1
+		if (self.div_refresh_values <= 0):
+			self.div_refresh_values=5
+
+			if beat:
+				# ~ parameters[PR].info = parameters[PR].info + u'❤'
+				parameters[PR].info = u'❤'
+				beat=False
+
+			for i in range(len(parameters)):
+				self.c_values[i].setText(parameters[i].getValueStr())
+				self.c_infos[i].setText(parameters[i].info)
+				self.setFrameStyleSheet(i)
+
+			if (time.time() > self.last_graphed_time+2):
+				self.last_graphed_time = time.time()
+
+				# update trends
+				self.trends_time.append(time.time())
+				for i in range(len(parameters)):
+					parameters[i].copyValueToTrends()
+
+				for pw in self.trendsPw:
+					pw.clear()
+
+				for i in range(len(parameters)):
+					self.trendsPw[parameters[i].plotid].plot(self.trends_time, parameters[i].trends, pen=parameters[i].color, connect='finite')
+
+		global X_fast_oximeter, Y_fast_oximeter, Y_fast_oximeter_index
+		self.oximeter.plot(X_fast_oximeter, Y_fast_oximeter, clear=True, pen=parameters[SPO2].color, connect='finite')
+
+	def setFrameStyleSheet(self, i):
+		if math.isnan(parameters[i].value):
+			self.c_frames[i].setStyleSheet("QGroupBox { border: 1px solid %s; border-radius: 8px; background-color: #000000; } QLabel { color: #555555; background-color: #000000;}" % (parameters[i].color));
+		elif (parameters[i].value < parameters[i].min or parameters[i].value > parameters[i].max):
+			self.c_frames[i].setStyleSheet("QGroupBox { border: 4px solid %s; border-radius: 8px; background-color: #ff0000; } QLabel { color: #000000; background-color: transparent;}" % parameters[i].color);
+		else:
+			self.c_frames[i].setStyleSheet("QGroupBox { border: 1px solid %s; border-radius: 8px; background-color: #000000; } QLabel { color: %s; background-color: #000000;}" % (parameters[i].color, parameters[i].color));
+
+	def keyPressEvent(self, event):
+		if (event.key() ==  Qt.Key_F11):
+			self.btnFullScreen()
+		else:
+			event.accept()
+
+	def eventFilter(self, source, event):
+		if event.type() == QEvent.MouseMove:
+			if event.buttons() == Qt.NoButton:
+				self.setCursor(Qt.ArrowCursor)
+				self.timerHideMouse.start(750)
+			#pos = event.pos()
+			#buttons = event.buttons()
+			#print('x: %d, y: %d' % (pos.x(), pos.y()), int(buttons))
+		return QMainWindow.eventFilter(self, source, event)
+
+	def btnFullScreen(self):
+		if (self.isFullScreen()):
+			self.showNormal()
+			#self.setCursor(Qt.ArrowCursor)
+		else:
+			self.showFullScreen()
+			#self.setCursor(Qt.BlankCursor)
+			self.timerHideMouse.start(100)
+
+	def timerHideMouseTimeout(self):
+		self.timerHideMouse.stop()
+		if (self.isFullScreen()):
+			self.setCursor(Qt.BlankCursor)
+
+	def timerClockTimeout(self):
+		val = time.strftime("%H:%M:%S", time.localtime(time.time()))
+		self.clockLabel.setText(val)
+
+	def btnReset(self):
+		self.trends_time = []
+		for i in range(len(parameters)):
+			parameters[i].trends = []
+		self.last_graphed_time = 0
+
+	def btnAutoRange(self):
+		for pw in self.trendsPw:
+			pw.enableAutoRange()
+
+	def closeEvent(self, event):
+		event.accept();return; # this line is overriding the confirmation
+
+		reply = QMessageBox.question(self, "Confirmation", "Are you sure to quit?", QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
+		if reply == QMessageBox.Yes:
+			event.accept()
+		else:
+			event.ignore()
+
+class DateAxis(pyqtgraph.AxisItem):
+	lastString = ""
+
+	def tickStrings(self, values, scale, spacing):
+		if len(values) > 1:
+			rng = max(values)-min(values)
+			if rng < 3600*24:
+				string = '%H:%M:%S'
+			elif rng >= 3600*24 and rng < 3600*24*30:
+				string = '%H:%M:%S\n%Y-%m-%d'
+			elif rng >= 3600*24*30 and rng < 3600*24*30*24:
+				string = '%Y-%m-%d'
+			elif rng >=3600*24*30*24:
+				string = '%Y'
+			self.lastString = string
+		else:
+			string = self.lastString
+
+		strns = []
+		for x in values:
+			try:
+				strns.append(time.strftime(string, time.localtime(x)))
+			except ValueError:  ## Windows can't handle dates before 1970
+				strns.append('')
+		return strns
+
+class DummyAxis(pyqtgraph.AxisItem):
+	def __init__(self, orientation):
+		super(DummyAxis, self).__init__(orientation)
+		self.setHeight(0)
+
+	def tickStrings(self, values, scale, spacing):
+		return ''
+
+def main():
+	global m1, oxi1
+	app = QApplication(sys.argv)
+	m1 = MonitorGUI()
+
+	oxi1 = usb_oximeter.Oximeter()
+	oxi1.assignSerialPorts(("/dev/ttyUSB0", "/dev/ttyUSB1"))
+	oxi1.setCallback(OximeterCallback)
+
+	threads = []
+	threads.append(threading.Thread(target=oxi1.worker))
+	threads[0].daemon = True
+	threads[0].start()
+
+	m1.last_graphed_time = 0
+	app.installEventFilter(m1)
+	ret = app.exec_()
+	oxi1.stop()
+	sys.exit(ret)
+
+if __name__ == '__main__':
+	main()
---
+++ usb_oximeter.py
@@ -0,0 +1,164 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import serial, sys, time, math
+
+class Oximeter:
+	def __init__(self, serialPort=None):
+		self.device = None; self.opened = False; self.serialport = None; self.exitLoop = False
+		self.serialPorts = (serialPort)
+		self.callback = None
+		self._resetParameters()
+
+		if serialPort:
+			self.open(serialPort)
+
+	def assignSerialPorts(self, serialPorts):
+		if serialPorts.__class__.__name__ in ('list', 'tuple'):
+			self.serialPorts = serialPorts
+		else:
+			self.serialPorts = [serialPorts]
+
+	def open(self, serialPort):
+		self.device = serial.Serial(serialPort, 19200, xonxoff=False, rtscts=False, dsrdtr=False, timeout=1, parity=serial.PARITY_ODD)
+		self.serialPort = serialPort
+		self.opened = True
+
+	def close(self):
+		if self.opened:
+			self.device.close()
+			self.opened = False
+			self.probeError = True
+
+	def stop(self):
+		self.exitLoop = True
+		self.probeError = True
+
+	def setCallback(self, callback):
+		self.callback = callback
+
+	def _resetParameters(self):
+		self.SpO2 = 0
+		self.pulseRate = 0
+		self.signalStrength = 0
+		self.waveformValue = 64
+		self.bargraphValue = 0
+		self.pulse = False
+		self.pulseLast = False
+		self.SpO2dropping = False
+		self.searching = False
+		self.searchingTooLong = False
+		self.probeError = True
+		self.beat_time=0
+		self.unfiltered_pulse_rate=0
+
+	def worker(self):
+		print "Thread started: OximeterWorker()"
+		count=-1
+		i = 0
+		t = 0
+		values=10*[0]
+		while(not self.exitLoop):
+			try:
+				if self.opened:
+					b = ord(self.device.read(1))
+					if (count>=0):
+						count+=1
+					if (bool(b&128)): # frame sync
+						count = 0
+					values[count] = b
+					if (count!=4):
+						continue
+				else:
+					if t>=math.pi*2:
+						t=0.05
+						# try to open the port!
+						if self.serialPorts is None:
+							continue
+						port = self.serialPorts[i]
+						i+=1
+						if i >= len(self.serialPorts):
+							i = 0
+						sys.stderr.write("Oximeter thread: trying to open: "+port+"\n")
+						self.open(port)
+						count=-1
+					else:
+						self.waveformValue = 64+32*math.sin(t)
+						self.signalStrength = 0
+						self.callback(0, 0)
+						time.sleep(0.01)
+						t+=0.05
+						continue
+
+			except KeyboardInterrupt:
+				self.stop()
+
+			except serial.SerialException:
+				sys.stderr.write("Oximeter SerialException: "+str(sys.exc_info()[1])+"\n")
+				self._resetParameters()
+				self.close()
+				self.probeError = True; count=-1;
+				self.callback(0, 0)
+				continue
+
+			except:
+				sys.stderr.write("Oximeter Error: "+str(sys.exc_info()[1])+"\n")
+				self._resetParameters()
+				self.close()
+				self.probeError = True; count=-1;
+				self.callback(0, 0)
+				continue
+
+			self.SpO2 = values[4]
+			self.pulseRate = values[3]
+			self.signalStrength = values[0]&15
+			self.waveformValue = values[1]&127
+			self.bargraphValue = values[2]&15
+			if values[0]&64:
+				if not self.pulseLast:
+					self.pulse = True
+				else:
+					self.pulse = False
+				self.pulseLast = self.pulse
+			else:
+				self.pulse = False
+				self.pulseLast = False
+			self.SpO2dropping = bool(values[0]&32)
+			self.searching = bool(values[2]&32)
+			self.searchingTooLong = bool(values[0]&16)
+			self.probeError = bool(values[2]&16)
+
+			self.callback(self.SpO2, self.pulseRate)
+		self.close()
+		print "Thread stopped: OximeterWorker()"
+
+
+def OximeterDemo(SpO2, pulse_rate):
+	global dev
+	sys.stdout.write("SpO²:%3d\tBPM:%4d" % (SpO2, pulse_rate))
+
+	if dev.pulse:
+		sys.stdout.write(" [beat]")
+
+	if dev.SpO2dropping:
+		sys.stdout.write(" [SpO² dropping]")
+
+	if dev.probeError:
+		sys.stdout.write(" [probe error]")
+
+	if dev.searching:
+		sys.stdout.write(" [searching signal]")
+
+	if dev.searchingTooLong:
+		sys.stdout.write(" [searching for too long]")
+
+	sys.stdout.write("\n")
+	sys.stdout.flush()
+
+if __name__ == '__main__':
+	global dev
+	dev = Oximeter()
+	dev.assignSerialPorts("/dev/ttyUSB0")
+	dev.setCallback(OximeterDemo)
+	dev.worker()
+	dev.stop()
